ab1a26b0bec8b8234c0ada29f1ba81f9498e2473
Muhammad Zubair <p19101042.muhammadzubair@gmail.com>
2026-09-01 22:15:16 +0500

    change Url

--- src/components/ResetPassword/api/auth.ts
+++ src/components/ResetPassword/api/auth.ts
@@ -1,22 +1,9 @@
 import axios from "axios";
-
-const BASE = "http://localhost:5000/api";
-// const BASE = "https://alburhan-backend-lyart.vercel.app/api";
-
-
+// 
+// const BASE = "http://localhost:5000/api";
+const BASE = "https://alburhan-backend-lyart.vercel.app/api";
 
 
-
-
-
-
-
-
-
-// const BASE = "http://192.9.210.50:5000/api";
-// const BASE = "https://alburhan-backend-production.up.railway.app/api";
-
-// const BASE = "https://alburhan-backend-production-32a2.up.railway.app/api";
 
 export const getUserByEmail = (email: string) =>
   axios.post(`${BASE}/auth/get-user-by-email`, { email });
